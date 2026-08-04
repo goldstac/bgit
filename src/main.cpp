9306ca550b07896519e346116f33a9517f0b1f02
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-04 22:47:13 +0400

    the lion does not concern himself with git commit messages

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,10 @@
 #include <cstdlib>
 int main(){
 std::string input;
-
+std::cout << "Welcome To BGIT\n";
+std::cout << "Options\n";
+std::cout << "[1] Add,Commit,Push\n";
+std::cout << "Enter Your Choice --> ";
+std::getline(std::cin,input);
+std::cout << input;
 }

d687c24e6f64b9ae683f0d4be46b17f21baf68e6
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-04 23:43:14 +0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,24 @@ std::cout << "Enter Your Choice --> ";
 std::getline(std::cin,input);
 std::cout << input;
 std::stringstream ss1;
+std::stringstream ss2;
+std::stringstream ss3;
+std::stringstream ss4;
 if (input == "1"){
     ss1 << "git add . && git commit -m \"" << commit_msg << "\" && git push";
     command = ss1.str();
     std::system(command.c_str());
+}
+else if (input == "2"){
+
+}
+else if (input == "3"){
+
+}
+else if (input == "3"){
+
+}
+else{
+    std::cout << "add a number\n";
 }
 }
